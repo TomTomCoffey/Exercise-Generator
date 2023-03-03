185134be9a950b32b12792cf42e7b206ad2461d0
Scott Klein <124641040+ScottKlein23@users.noreply.github.com>
2023-03-03 12:54:32 -0500

    Add files via upload
    OLD version for practice with database

--- CalculateBMI.php
+++ CalculateBMI.php
@@ -3,6 +3,30 @@
 
 <body>
 <?php
+    
+    /*for code to work create a database in phpMyAdmin called Users with a 
+    with a table called User with these attributes:
+        name (text)
+        email (text)
+        password (text)
+        height (int)
+        weight(int)
+        BMI (float)
+    */
+
+    $serverName = "localhost";
+    $userName = "root"; 
+    $password = ""; 
+    $dbName = "Users";
+
+    
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+    if($conn){
+        echo "connected<br>";
+    }
+    else{
+        echo "no connection<br>";
+    }
 
     $name = $_POST["name"];
 
@@ -53,7 +77,14 @@
         $bmi = 703 * ($weight / ($height * $height));
         echo "<br>Your bmi is " . number_format((float)$bmi, 2, '.', '');
     }
-
+    
+    $sql = "insert into User(name,email,password,height,weight,bmi) values('$name','$email','$password','$height','$weight','$bmi')";
+  
+    if ($conn->query($sql) === TRUE) {
+        echo "ADDED: ";
+    } else {
+        echo "Error: ";
+    }
     
 ?>
 
