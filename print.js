47d25cfaa34b903fe3722cbf648e773055254437
Brian Ordonez <bordonez1129@gmail.com>
2023-03-22 18:58:24 -0400

    Brian JS

---
+++ print.js
@@ -0,0 +1,18 @@
+
+document.getElementById("submission").onclick = function()
+{
+    var exType = document.getElementById("eT").value;
+    var numSet = document.getElementById("nS").value;
+    var numRep = document.getElementById("nR").value;
+    var weight = document.getElementById("wht").value;
+
+    var output1 = document.getElementById("out1");
+    var output2 = document.getElementById("out2");
+    var output3 = document.getElementById("out3");
+    var output4 = document.getElementById("out4");
+
+    output1.innerHTML = exType;
+    output2.innerHTML = numSet;
+    output3.innerHTML = numRep;
+    output4.innerHTML = weight;
+}
