c477693556c8c90280b2e7f112026df062c26838
ScottKlein23 <kleins434@gmail.com>
2023-04-02 16:44:32 -0400

    added php functions to login

---
+++ Client/src/views/ShouldersView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <p>shoulders page</p>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ Client/src/views/SignUpView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <p>signup page</p>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
