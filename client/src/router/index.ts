22f955163ed2401f811b6b95e184ca7e4b2752fb
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-04-02 16:31:04 -0400

    Adjustments to vue, bulma is now imported

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -5,11 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+      path: '/', name: 'home',component: HomeView },
+      
     {
+    
       path: '/about',
       name: 'about',
       // route level code-splitting
