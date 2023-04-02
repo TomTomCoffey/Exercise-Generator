6f373d6c6b788a1780f2162648ff0fb3ea10308f
ScottKlein23 <kleins434@gmail.com>
2023-04-02 16:44:52 -0400

    created views

---
+++ Client/src/views/ArmsView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+
+        <p> testing</p>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ Client/src/views/ChestView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <p>chest page</p>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
