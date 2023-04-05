f0ab97f78ea94cd93ccfbe424aef23b574bfb07a
Brian Ordonez <bordonez1129@gmail.com>
2023-04-05 18:15:03 -0400

    Adding Images to Workouts - Brian

--- Client/src/views/BackView.vue
+++ Client/src/views/BackView.vue
@@ -2,14 +2,61 @@
 
 </script>
 <template>
-    <div>
+    <body>
+    <div class = "block pt-5"></div>
+      <div class = "columns">
+        <div class = "column is is-three-fifths is-offset-one-fifth is narrow-mobile">
+          <div class = "block pt-5"></div>
+            <div class = "box mb-3">
+            <div class = "columns">
+              <div class = "column is-half">
+                <h1 class = "title">Back</h1>
+                <p>
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                </p>
 
-        <p> <br><br><br><br><br>This is the back page</p>
 
-    </div>
+              </div>
+
+              <div class = "column is-half">
+                <h1 class = "title has-text-centered">Location</h1>
+                <img src = "../assets/images/backRed.png">
+
+
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
 </template>
 
 
 <style scoped>
-
+    body
+    {
+        background-color: rgba(0, 149, 255, 0.167);
+    }
+    .box
+    {
+        box-shadow: 1px 1px 6px darkgray;
+    }
 </style>
--- Client/src/views/ShouldersView.vue
+++ Client/src/views/ShouldersView.vue
@@ -2,13 +2,62 @@
 
 </script>
 <template>
-    <div>
-        <p><br><br><br><br>shoulders page</p>
-    </div>
+    <body>
+    <div class = "block pt-5"></div>
+      <div class = "columns">
+        <div class = "column is is-three-fifths is-offset-one-fifth is narrow-mobile">
+          <div class = "block pt-5"></div>
+            <div class = "box mb-3">
+            <div class = "columns">
+              <div class = "column is-half">
+                <h1 class = "title">Shoulders</h1>
+                <p>
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                </p>
+
+
+              </div>
+
+              <div class = "column is-half">
+                <h1 class = "title has-text-centered">Location</h1>
+                <img src = "../assets/images/shouldersRed.png">
+
+
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
 </template>
 
 
 
 <style scoped>
-
+    body
+    {
+        background-color: rgba(0, 149, 255, 0.167);
+    }
+    .box
+    {
+        box-shadow: 1px 1px 6px darkgray;
+    }
 </style>
