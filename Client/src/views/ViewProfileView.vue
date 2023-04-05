23e708ebac76ca071f854fa331a87d5cd9496431
Brian Ordonez <bordonez1129@gmail.com>
2023-04-05 19:27:43 -0400

    View Profile - Brian

---
+++ Client/src/views/ViewProfileView.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+
+</script>
+<template>
+   <body id = "grad3">
+    <div class = "block pt-5"></div>
+      <div class = "columns">
+        <div class = "column is is-three-fifths is-offset-one-fifth is narrow-mobile">
+          <div class = "block pt-5"></div>
+            <div class = "box mb-3">
+            <div class = "columns">
+              <div class = "column is-half">
+                <h1 class = "title">View Profile</h1>
+                <p>
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                  There will be words here, that everyone will be able to read.
+                </p>
+
+
+              </div>
+
+              <div class = "column is-half">
+                <h1 class = "title has-text-centered">Brian Ordonez</h1>
+                <img src = "../assets/images/BrianOrdonez.jpg">
+
+
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</template>
+
+<style>
+ body
+    {
+        background-color: rgba(0, 149, 255, 0.167);
+    }
+    .box
+    {
+        box-shadow: 1px 1px 6px darkgray;
+    }
+    #grad3 
+    {
+      background-image: linear-gradient(to right, rgb(246, 255, 0) ,  rgb(255, 153, 0), rgba(246, 255, 0));
+    }
+</style>
