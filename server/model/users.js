9170b55890468277118d8e07937bfe1a359174b4
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-04-16 15:04:24 -0400

    Started users model

---
+++ server/model/users.js
@@ -0,0 +1,43 @@
+const data = require('../data/users.json')
+
+
+function getUsers() {
+  return data.users;
+}
+
+function getUserbyId(id) {
+    return data.users.find(user => user.id === id);
+}
+
+function addUser(user) {
+    user.id = data.users.length + 1;
+    data.users.push(user);
+}
+
+function updateUser(user) {
+    const index = data.users.findIndex(p => p.id === user.id);
+    data.users[index] = user;
+}
+
+function deleteUser(id) {
+    const index = data.users.findIndex(p => p.id === id);
+    data.users.splice(index, 1);
+}
+
+function searchUsers(searchTerm) {
+    return data.users.filter(user => {
+        return  user.name.toLowerCase().includes(searchTerm.toLowerCase())  ||
+            user.email.toLowerCase().includes(searchTerm.toLowerCase())  ||
+            user.password.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+}
+
+module.exports = {
+    getUsers,
+    getUserbyId,
+    addUser,
+    updateUser,
+    deleteUser,
+    searchUsers
+
+};
