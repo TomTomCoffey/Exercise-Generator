f4b7205c2fa247d134db0228f4cd3068796bfdb8
Brian Ordonez <bordonez1129@gmail.com>
2023-04-17 19:51:07 -0400

    Changes - Brian

--- Client/src/views/SignUpView.vue
+++ Client/src/views/SignUpView.vue
@@ -2,13 +2,75 @@
 
 </script>
 <template>
+   <body id = "grad">
     <div>
-        <p>signup page</p>
+
+        <div class = "columns">
+    <div class  = "column is-half is-offset-one-quarter">
+      <div class="block pt-5"></div>
+      <div class="block pt-5"></div>
+      <div class="block pt-5"></div>
+     
+        <form class="box" action="login.php" method="POST">
+          <h1 class="title has-text-centered">Log in and find your workout for today!</h1>
+          <h2 class="subtitle has-text-centered">
+            If you do not have an account please sign up.
+          </h2>
+            
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input name="email" class="input" type="email" placeholder="e.g. alex@example.com">
+            </div>
+          </div>
+  
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input name="password" class="input" type="password" placeholder="********">
+            </div>
+          </div>
+
+          <button class="button is-info is-outlined is-link" href = "">Sign Up</button>
+          <button class="button is-info is-outlined is-success ml-3" @click="login()">Log In</button>
+          <label class="checkbox is-pulled-right">
+          <input type="checkbox"> Remember me</label>
+        </form>
+  </div>
+  </div>
+  <div class="block pt-5"></div>
+  <div class="block pt-5"></div>
+  <div class="block pt-5"></div>
+
+
+
+
+
     </div>
+  </body>
 </template>
 
 
 
 <style scoped>
-
+          .body
+          {
+            background-color: rgba(0, 149, 255, 0.64);
+          }
+          .navbar
+          {
+            background-color: white;
+          }
+          .footer
+          {
+            background-color: rgb(255, 139, 0)
+          }   
+          .box
+          {
+            box-shadow: 1px 1px 6px darkgray;
+          }
+          #grad 
+          {
+            background-image: linear-gradient(to left, rgb(255, 139, 0) , rgb(2, 0, 198));
+          }
 </style>
