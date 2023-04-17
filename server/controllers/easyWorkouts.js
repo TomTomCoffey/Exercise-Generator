9572ea7baa7314ea752a6659d7c2125c6ac9e717
ScottKlein23 <kleins434@gmail.com>
2023-04-17 17:25:58 -0400

    Working on model/server

---
+++ server/controllers/easyWorkouts.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const model = require('../models/easyWorkouts');
+const router = express.Router();
+
+router
+
+.get('/', (req, res) => {
+    const list = model.getEasyWorkouts();
+    const data = { data: list, total: list.length, isSuccess: true };
+    res.send(data)
+})
+
+
+.get('/', (req, res) => {
+    const list = model.getEasyWorkouts();
+    const data = { data: list, total: list.length, isSuccess: true };
+    res.send(data)
+})
+
