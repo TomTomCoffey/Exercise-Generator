c133d341133368de274b8b7a99f4d4205eba736f
ScottKlein23 <kleins434@gmail.com>
2023-04-24 19:24:22 -0400

    Connection for DB

---
+++ server/MongoDB/DbConnection.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function connect(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://scott_klein23:Scottscott123@cluster0.uxbryam.mongodb.net/test";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+connect().catch(console.error);
