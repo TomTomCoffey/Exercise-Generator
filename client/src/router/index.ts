801b0fa75a1842ff06114657ecf3d2ba57292902
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-05-06 15:58:22 -0400

    routing puts login page up to start

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ArmsViewVue from '@/views/ArmsView.vue'
 import ChestViewVue from '@/views/ChestView.vue'
@@ -12,6 +13,7 @@ import AbsViewVue from '@/views/AbsView.vue'
 import SettingsViewVue from '@/views/SettingsView.vue'
 import ViewProfileViewVue from '@/views/ViewProfileView.vue'
 import SignUpViewVue from '@/views/SignUpView.vue'
+import { useSession } from '@/model/user'
 
 
 
@@ -19,18 +21,18 @@ import SignUpViewVue from '@/views/SignUpView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/', name: 'home',component: HomeView },
+    {path: '/', name: 'home',component: HomeView, beforeEnter: secureRoute },
     {path: '/arms', name: 'arms',component: ArmsViewVue },
     {path: '/chest', name: 'chest',component: ChestViewVue },
     {path: '/back', name: 'back',component: BackViewVue },
     {path: '/shoulders', name: 'shoulders',component: ShouldersViewVue },
     {path: '/cardio', name: 'cardio',component: CardioViewVue },
-    {path: '/personal records', name: 'personal records',component: PersonalRecordsViewVue },
+    {path: '/personal records', name: 'personal records',component: PersonalRecordsViewVue,  beforeEnter: secureRoute},
     {path: '/legs', name: 'legs',component: LegsView },
     {path: '/abs', name: 'abs',component: AbsViewVue },
     {path: '/login', name: 'login',component: LoginViewVue },
-    {path: '/settings', name: 'settings',component: SettingsViewVue },
-    {path: '/viewProfile', name: 'viewProfile',component: ViewProfileViewVue },
+    {path: '/settings', name: 'settings',component: SettingsViewVue, beforeEnter: secureRoute },
+    {path: '/viewProfile', name: 'viewProfile',component: ViewProfileViewVue, beforeEnter: secureRoute },
     {path: '/signUp', name: 'signUp', component: SignUpViewVue },
  
     {
@@ -48,5 +50,16 @@ const router = createRouter({
 
 
 export default router
+
+
+
+function secureRoute (to : RouteLocationNormalized, from : RouteLocationNormalized, next : NavigationGuardNext ) {
+  const session = useSession();
+  if (session.user) {
+      next()
+  } else { 
+      next('/login')
+  }
+}
 
 
