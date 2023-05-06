2c5690910e9b75ca77da305a83f91ed7fb2bb0d7
Brian Ordonez <bordonez1129@gmail.com>
2023-05-06 15:09:37 -0400

    changes - Brian

--- Client/src/views/BackView.vue
+++ Client/src/views/BackView.vue
@@ -17,26 +17,13 @@
                 </ul>
               </nav>
             <div class = "box mb-3">
-            <div class = "columns">
+            <div class = "columns col">
               <div class = "column is-half">
-                <h1 class = "title">Back</h1>
-                <p>
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
-                  There will be words here, that everyone will be able to read.
+                <h1 class = "title has-text-centered"><u>The Back</u></h1>
+                <p class = "is-size-4">
+                  The back is made up of 3 main muscles. The Lats, Rhomboids,
+                  and the Trapezius. It is located on your backside and is shown
+                  highlighted in red in the image to the right.
                 </p>
 
 
@@ -49,20 +36,20 @@
 
               </div>
             </div>
-            <h1 class = "title has-text-centered">Arm Exercises</h1>
+            <h1 class = "title has-text-centered">Back Exercises</h1>
             <hr class="navbar-divider-black">
             <div class = "columns col">
               <div class = "column is-half">
-                <figure class= "image">
-                  <img src = "../assets/images/bicepCurl.gif">
+                <figure class= "image is-3by4">
+                  <img src = "../assets/images/latdown.gif">
                 </figure>
               </div>
 
               <div class = "column is-half">
-                <h1 class = "title has-text-centered image">Bicep Curls</h1>
+                <h1 class = "title has-text-centered image"><u>Pull Downs</u></h1>
                 <div>
-                <p>
-                  There will be words here, that everyone will be able to read.
+                <p class = is-size-4>
+                  <strong><i>Pull Downs:</i></strong> There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
@@ -77,10 +64,10 @@
 
             <div class = "columns col">
               <div class = "column is-half">
-                <h1 class = "title has-text-centered image">Hammer Curls</h1>
+                <h1 class = "title has-text-centered image"><u>Cable Rows</u></h1>
                 <div>
-                <p>
-                  There will be words here, that everyone will be able to read.
+                <p class = "is-size-4">
+                  <strong><i>Cable Rows:</i></strong> There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
                   There will be words here, that everyone will be able to read.
@@ -93,8 +80,8 @@
             </div>
 
               <div class = "column is-half">
-                <figure class= "image">
-                  <img src = "../assets/images/hammerCurl.gif">
+                <figure class= "image is-3by4">
+                  <img src = "../assets/images/cableRow.gif">
                 </figure>
               </div>
               </div>
@@ -119,5 +106,10 @@
     #grad2 
     {
       background-image: linear-gradient(to right, rgb(255, 102, 0) ,  rgb(255, 0, 0), rgba(255, 102, 0));
-    }
+    }  
+    .col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
